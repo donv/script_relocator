0e0c14aac0f7ea591071d6b384512649b3e90218
donv <uwe@kubosch.no>
2018-12-03 00:59:32 +0100

    Only process docs with DOCTYPE (leave fragments alone) Leave scripts with `async` or `defer` alone since they are handled by the browser

--- lib/script_relocator.rb
+++ lib/script_relocator.rb
@@ -13,15 +13,18 @@ module ScriptRelocator
       end
       response_body = ''
       response.each { |part| response_body << part }
+      return status, headers, response unless response_body =~ /\A<!DOCTYPE/
       doc = Nokogiri::HTML(response_body)
+      target = doc.at('body')
       scripts = doc.css('script')
       return status, headers, response if scripts.empty?
       scripts.each do |s|
+        next if s['async'] == 'async' || s['defer'] == 'defer'
         s['data-turbolinks-eval'] = 'false' if s.parent.name == 'head'
         s.remove
-        doc.at('body') << s
+        target << s
       end
-      transformed_body = doc.canonicalize
+      transformed_body = doc.to_html(save_with: Nokogiri::XML::Node::SaveOptions::NO_DECLARATION | Nokogiri::XML::Node::SaveOptions::NO_EMPTY_TAGS | Nokogiri::XML::Node::SaveOptions::AS_HTML)
       if headers.key?('Content-Length') &&
           headers['Content-Length'].to_i != transformed_body.length
         headers['Content-Length'] = transformed_body.length.to_s
--- test/script_relocator_test.rb
+++ test/script_relocator_test.rb
@@ -17,10 +17,18 @@ class ScriptRelocatorTest < Minitest::Test
 
   def test_script_is_moved
     sl = ScriptRelocator::Rack.new -> env do
-      [200, {'Content-Type' => 'text/html'}, ['<html><head></head><body><script></script>Success</body></html>']]
+      [200, {'Content-Type' => 'text/html'}, ['<!DOCTYPE html><html><head></head><body><script></script>Success</body></html>']]
     end
     result = sl.call({})
-    assert_equal [200, {'Content-Type' => 'text/html'}, ['<html><head></head><body>Success<script></script></body></html>']], result
+    assert_equal [200, {'Content-Type' => 'text/html'}, ["<!DOCTYPE html>\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></head><body>Success<script></script></body></html>\n"]], result
+  end
+
+  def test_fragments_are_preserved
+    sl = ScriptRelocator::Rack.new -> env do
+      [200, {'Content-Type' => 'text/html'}, ['<script></script><span>Fragment</span>']]
+    end
+    result = sl.call({})
+    assert_equal [200, {'Content-Type' => 'text/html'}, ['<script></script><span>Fragment</span>']], result
   end
 
   def test_script_non_html_content_type_is_not_changed
@@ -31,11 +39,54 @@ class ScriptRelocatorTest < Minitest::Test
     assert_equal [200, {}, '<html><body><script></script>Success</body></html>'], result
   end
 
-  def test_script_double_quotes_are_preserved
+  def test_double_quotes_are_preserved
     sl = ScriptRelocator::Rack.new -> env do
-      [200, {'Content-Type' => 'text/html'}, [%q{<html><body><tag attr='"'/></html>}]]
+      [200, {'Content-Type' => 'text/html'}, [%q{<html><body><tag attr='"'/></body></html>}]]
     end
     result = sl.call({})
-    assert_equal [200, {'Content-Type' => 'text/html'}, [%q{<html><body><tag attr='"'/></html>}]], result
+    assert_equal [200, {'Content-Type' => 'text/html'}, [%q{<html><body><tag attr='"'/></body></html>}]], result
+  end
+
+  def test_unicode_chars_are_preserved_in_document
+    sl = ScriptRelocator::Rack.new -> env do
+      [200, {'Content-Type' => 'text/html'}, [%q{<!DOCTYPE html><html><body><script/><span>×</span></body></html>}]]
+    end
+    result = sl.call({})
+    assert_equal [200, {'Content-Type' => 'text/html'}, [%Q{<!DOCTYPE html>\n<html><body><span>×</span><script></script></body></html>\n}]], result
+  end
+
+  def test_unicode_chars_are_preserved
+    sl = ScriptRelocator::Rack.new -> env do
+      [200, {'Content-Type' => 'text/html'}, [%q{<!DOCTYPE html><html><body><script/><span>×</span></body></html>}]]
+    end
+    result = sl.call({})
+    assert_equal [200, {'Content-Type' => 'text/html'}, [%Q{<!DOCTYPE html>\n<html><body><span>×</span><script></script></body></html>\n}]], result
+  end
+
+  def test_entities_are_preserved
+    sl = ScriptRelocator::Rack.new -> env do
+      [200, {'Content-Type' => 'text/html'}, [%q{<!DOCTYPE html><html><body><script/><span>&times;</span></body></html>}]]
+    end
+    result = sl.call({})
+    assert_equal [200, {'Content-Type' => 'text/html'}, [%Q{<!DOCTYPE html>\n<html><body><span>×</span><script></script></body></html>\n}]], result
+  end
+
+  def test_ampersands_are_preserved
+     html = %q{<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><script>
+  function checkOtherOption(e, tf) {
+    var other_selected = ($(e).val() && ($(e).val().indexOf('annet') > -1));
+    tf.prop('disabled', !other_selected);
+    if (other_selected) {
+        tf.show().focus()
+    } else {
+        tf.hide()
+    }
+  }
+</script></body></html>
+}
+    sl = ScriptRelocator::Rack.new -> env { [200, { 'Content-Type' => 'text/html' }, [html]] }
+    result = sl.call({})
+    assert_equal [200, {'Content-Type' => 'text/html'}, [html]], result
   end
 end
